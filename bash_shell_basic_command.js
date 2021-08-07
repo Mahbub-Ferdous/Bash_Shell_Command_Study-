5e7c0c648bc23617340da5c4d81ddb48004940cb
Mahbub-Ferdous <mahbubferdous14@gmail.com>
2021-08-07 15:10:24 +0600

    Bash basic command

---
+++ bash_shell_basic_command.js
@@ -0,0 +1,113 @@
+********************  pwd, ls, cd, mkdir  ********************
+
+
+
+present wprking directory see command -------  pwd
+
+For see any kinds of file list  ----- ls
+
+changing directory command is -------    cd directory name
+
+making new directory command is -----  mkdir making fileName
+
+if you want to create a file directory on directory then file then command ---  mkdir -p directoryName/folderName/fileName
+
+
+
+**********************  Ctrl, Alt, exit, clear  ******************
+
+
+for terminal clear command is -------  clear
+
+if you wnat to exit the terminal then simply command  ----- exit
+
+for command line scrolling to first command -----  Ctrl+l
+
+for go to command line first word then command --- Ctrl+a
+
+then for go to command line end command ------ Ctrl+e
+
+if you want to back one word then command  ----  Alt + b   [b means backward]
+
+if you want to forward one then command ------ Alt + f     [ f means forward]
+
+if you want to back one alphabet then command  ----- Ctrl + b         [backward]
+
+if you want to forward one alphabet then command --- Ctrl + f         [forward]
+
+if you want to delete backward one word then command  ---- Alt + backspacing
+
+if you want to delete backword one word then command  -----  Alt + d
+
+
+
+**************************  touch, cat, less, gedit, vim, whoami, date, time  *************************
+
+
+
+for open a file on directory then go to the cd command to this directory then command ---- touch fileName
+
+for read any file then command ----- cat fileName  or  less fileName 
+
+if you want to see head side line of file command  ------ head fileName
+
+if you want to see tail side line of the file command ---  tail fileName  
+
+open a file with gedit editor or vim editor -----  gedit fileName  and vim fileName 
+
+see your pc name then command ---- whoami
+
+see todays date command ---- date
+
+see time then command  ------ time 
+
+if you want to open calender then command  --- cal
+
+if you want to see full year calender command  ---  cal year
+
+
+
+**************************  cat,cd .. ,code . ,rm,mv,cp  **************************
+
+
+
+
+if you see more command which you dont know then command   ------- cat --h 
+
+once you go to a directory to another directory but want to go agin first directory then command  ----  cd .. 
+
+if you want to go again past directory then command   ---- cd ../..
+
+if you want to open your vscode editor to write some code then simply command on your bash ------   code .
+
+for open your google chrome from your bash command then command --- google-chrome  
+
+if you remove any file then command  ----  rm fileName
+
+if you remove any directory then command  -----  rm -rf directoryName 
+
+if you want to rename any file or directory then simply command -----  mv oldFileName newFileName
+
+if you move any file file one repo to another repo then command   ----- mv fileName Repository name
+
+if copy somethinng one file to another file then command -----  cp fromFileName toFileName 
+
+
+
+
+
+if you need something bigger then command  ----- man , then command ---- man which you want 
+
+if i want to take some code to githb file raw then create a file then goto the file command ----- curl -O rawLink, [that called download code with curl command].
+
+
+
+ 
+
+
+
+ 
+
+
+
+
